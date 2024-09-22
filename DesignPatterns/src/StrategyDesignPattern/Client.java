8c9f3f613b819ac35691806503e2b8703e073707
mkhulbe5 <chandmohit163@gmail.com>
2024-09-22 19:29:04 +0530

    minor changes

--- DesignPatterns/src/StrategyDesignPattern/Client.java
+++ DesignPatterns/src/StrategyDesignPattern/Client.java
@@ -2,7 +2,8 @@ package StrategyDesignPattern;
 
 public class Client {
 	public static void main(String[] args) {
-		SportsVehicle c = new SportsVehicle();
-		c.drive();
+		// SportsVehicle c = new SportsVehicle();
+		// c.drive();
+		System.out.println("test");
 	}
 }
